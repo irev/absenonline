c73bc208b4a44f8408d664d5a550450d5bad959d
Refyandra <irev@users.noreply.github.com>
2022-10-20 19:48:02 +0700

    fix Form input daftar user

--- application/views/v_buat_absen.php
+++ application/views/v_buat_absen.php
@@ -83,7 +83,7 @@
                                         <!-- <button type="submit" class="btn btn-primary">Submit</button> -->
                                     </form>
                                     <h4>Daftar Pengajuan <span id="jumlah-form"></span></h4>
-                                    <form id="form-multiple" class="section-to-print" enctype="multipart/form-data" method="post" target="<?= base_url('buatabsen_controller/PengajuanCreate/').$this->uri->segment('3'); ?>">
+                                    <form id="form-multiple" class="section-to-print" enctype="multipart/form-data" method="post"  ?>">
                                         <table class="table form-sementara" id="form-sementara">
                                             <thead>
                                                 <tr>
