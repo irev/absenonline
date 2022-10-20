97e65394e86c4fcb12d6e9874dddd7b3d0501836
Refyandra <irev@users.noreply.github.com>
2022-10-20 18:12:24 +0700

    form to a

--- application/controllers/Buatabsen_controller.php
+++ application/controllers/Buatabsen_controller.php
@@ -203,7 +203,7 @@ class Buatabsen_controller extends CI_Controller{
     }
 
     function RequestItemForm($idx=null){
-        dd($idx);
+        var_dump($idx);
         if($idx == null){
            redirect('/buatabsen_controller');
         }
